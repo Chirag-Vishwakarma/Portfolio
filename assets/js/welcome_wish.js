eefb6719eba1028ce03e599fdc3db91d7803ab43
Chirag <chiragvishwakarma@gmail.com>
2026-06-04 09:20:04 +0530

    Initial addition of Git in Project

---
+++ assets/js/welcome_wish.js
@@ -0,0 +1,15 @@
+let hour = new Date().getHours();
+let greeting;
+
+if (hour < 5) {
+    greeting = 'Hello..!🖐';
+} else if (hour < 12) {
+    greeting = 'Good Morning..!☕';
+} else if (hour < 16) {
+    greeting = 'Good Afternoon..!😃';
+} else if (hour < 20) {
+    greeting = 'Good Evening..!🌙';
+} else {
+    greeting = 'Hello..!🖐';
+}
+window.alert(greeting + " Welcome to Chirag's Portfolio..❤");
